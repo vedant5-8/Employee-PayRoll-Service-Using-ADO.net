5f673e6f5d7ae806306c7d71a0c9060d903d9ab3
Vedant <vedantnp200@gmail.com>
2023-06-29 12:55:24 +0530

    [Vedant]-New records inserted usng ADO.net

--- Employee_Payroll_Controller.cs
+++ Employee_Payroll_Controller.cs
@@ -58,5 +58,59 @@ namespace Employee_Payroll_Service
             }
         }
 
+        public static void InsertRecords()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(@"data source=DESKTOP-4VPJFH9\SQLEXPRESS;initial catalog=EmployeePayrollService;integrated security=true");
+                Employee_Payroll_Model model = new Employee_Payroll_Model();
+                Console.WriteLine("Enter Name: ");
+                model.Name = Console.ReadLine();
+                Console.WriteLine("Enter Salary: ");
+                model.Salary = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Start Date in given format (yyyy-MM-dd):");
+                model.StartDate = Convert.ToDateTime(Console.ReadLine());
+                Console.WriteLine("Enter Gender: ");
+                model.Gender = Console.ReadLine();
+                Console.WriteLine("Enter Phone: ");
+                model.Phone = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine("Enter Address: ");
+                model.Address = Console.ReadLine();
+                Console.WriteLine("Enter Department: ");
+                model.Department = Console.ReadLine();
+                Console.WriteLine("Enter Deduction: ");
+                model.Deduction = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Taxable pay: ");
+                model.Taxable_Pay = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Income Tax: ");
+                model.Income_Tax = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Net Pay: ");
+                model.Net_Pay = Convert.ToInt32(Console.ReadLine());
+                con.Open();
+
+                string Query = "INSERT INTO Employee_Payroll VALUES (" +
+                    "'" + model.Name + "', " +
+                    "'" + model.Salary + "'," +
+                    "'" + model.StartDate + "'," +
+                    "'" + model.Gender + "'," +
+                    "'" + model.Phone + "'," +
+                    "'" + model.Address + "'," +
+                    "'" + model.Department + "'," +
+                    "'" + model.Deduction + "'," +
+                    "'" + model.Taxable_Pay + "'," +
+                    "'" + model.Income_Tax + "'," +
+                    "'" + model.Net_Pay + "');";
+
+                SqlCommand cmd = new SqlCommand(Query, con);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Record Inserted Successfully.");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,4 +3,6 @@ using Employee_Payroll_Service;
 
 // Employee_Payroll_Controller.CreateDatabase();
 
-Employee_Payroll_Controller.CreateTable();
+// Employee_Payroll_Controller.CreateTable();
+
+Employee_Payroll_Controller.InsertRecords();
