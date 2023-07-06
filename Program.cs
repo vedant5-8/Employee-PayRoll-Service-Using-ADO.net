278315967a024872ba224d8536260a0dacd1962d
Vedant <vedantnp200@gmail.com>
2023-07-06 10:43:55 +0530

    [Vedant]-Create new table

--- Program.cs
+++ Program.cs
@@ -3,7 +3,4 @@ using Employee_Payroll_Service;
 
 // Employee_Payroll_Controller.CreateDatabase();
 
-// Employee_Payroll_Controller.CreateTable();
-
-Employee_Payroll_Controller.InsertRecord();
-
+Employee_Payroll_Controller.CreateTable();
