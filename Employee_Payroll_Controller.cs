001fae71b5e6547e280cef2d3c898a565af0bc0a
Vedant <vedantnp200@gmail.com>
2023-07-06 10:57:08 +0530

    [Vedant]-Insert new record to the Employee_Payroll Table

--- Employee_Payroll_Controller.cs
+++ Employee_Payroll_Controller.cs
@@ -61,5 +61,81 @@ namespace Employee_Payroll_Service
             }
         }
 
+        // UC3: Insert data in Employee_Payroll table
+
+        public static void InsertRecord()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(@"data source=DESKTOP-4VPJFH9\SQLEXPRESS;initial catalog=EmployeePayrollService;integrated security=true");
+                con.Open();
+
+                Employee_Payroll_Model model = new Employee_Payroll_Model();
+
+                Console.Write("Enter Name: ");
+                model.EmpName = Console.ReadLine();
+
+                Console.Write("Enter Phone Number: ");
+                model.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+
+                Console.Write("Enter Gender: ");
+                model.Gender = Console.ReadLine();
+
+                Console.Write("Enter Start Date in format (yyyy-MM-dd):");
+                model.StartDate = Convert.ToDateTime(Console.ReadLine());
+
+                Console.Write("Enter Address: ");
+                model.Address = Console.ReadLine();
+
+                Console.Write("Enter City: ");
+                model.City = Console.ReadLine();
+
+                Console.Write("Enter State: ");
+                model.State = Console.ReadLine();
+
+                Console.Write("Enter Department: ");
+                model.Department = Console.ReadLine();
+
+                Console.Write("Enter Basic Pay: ");
+                model.BasicPay = Convert.ToSingle(Console.ReadLine());
+
+                Console.Write("Enter Deduction: ");
+                model.Deduction = Convert.ToSingle(Console.ReadLine());
+
+                Console.Write("Enter Taxable Pay: ");
+                model.TaxablePay = Convert.ToSingle(Console.ReadLine());
+
+                Console.Write("Enter Income Tax: ");
+                model.IncomeTax = Convert.ToSingle(Console.ReadLine());
+
+                String Query = "INSERT INTO Employee_Payroll VALUES " +
+                    "(@Emp_Name, @Phone_Number, @Gender, @Start_Date, @Address, @City, @State, @Department, @Basic_Pay, @Deduction, @Taxable_Pay, @Income_Tax)";
+
+                SqlCommand cmd = new SqlCommand(Query, con);
+
+                cmd.Parameters.AddWithValue("@Emp_Name", model.EmpName);
+                cmd.Parameters.AddWithValue("@Phone_Number", model.PhoneNumber);
+                cmd.Parameters.AddWithValue("@Gender", model.Gender);
+                cmd.Parameters.AddWithValue("@Start_Date", model.StartDate);
+                cmd.Parameters.AddWithValue("@Address", model.Address);
+                cmd.Parameters.AddWithValue("@City", model.City);
+                cmd.Parameters.AddWithValue("@State", model.State);
+                cmd.Parameters.AddWithValue("@Department", model.Department);
+                cmd.Parameters.AddWithValue("@Basic_Pay", model.BasicPay);
+                cmd.Parameters.AddWithValue("@Deduction", model.Deduction);
+                cmd.Parameters.AddWithValue("@Taxable_Pay", model.TaxablePay);
+                cmd.Parameters.AddWithValue("@Income_Tax", model.IncomeTax);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+                Console.WriteLine("{0} rows are inserted.", rowsAffected);
+                Console.WriteLine("Record Inserted Successfully.");
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
     }
 }
