fed4e4f45689bc6cafd5da9af4abd1f2b92f07d4
Vedant <vedantnp200@gmail.com>
2023-07-09 22:07:26 +0530

    [Vedant]-

--- Employee-Payroll-Service-Project/Employee_Payroll_Model.cs
+++ Employee-Payroll-Service-Project/Employee_Payroll_Model.cs
@@ -8,14 +8,6 @@ namespace Employee_Payroll_Service
         public long PhoneNumber { get; set; }
         public string Gender { get; set; }
         public DateTime StartDate { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string Department { get; set; }
-        public double BasicPay { get; set; }
-        public double Deduction { get; set; }
-        public double TaxablePay { get; set; }
-        public double IncomeTax { get; set; }
-        public double NetPay { get; set; }
+        public long Salary { get; set; }
     }
 }
